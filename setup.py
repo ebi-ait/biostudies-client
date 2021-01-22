ba33f59ed3a1d828fcbc94788d363bd79a5a56ec
Karoly Erdos <karoly@ebi.ac.uk>
2021-01-22 13:57:09 +0000

    Add configuration file for deployment

---
+++ setup.py
@@ -0,0 +1,16 @@
+""" Configuration file for deployment """
+
+import os
+
+from setuptools import setup, find_packages
+
+base_dir = os.path.dirname(__file__)
+install_requires = [line.rstrip() for line in open(os.path.join(base_dir, 'requirements.txt'))]
+
+setup(
+    name='biostudies-client',
+    version='0.1.0',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    install_requires=install_requires,
+    include_package_data=True
+)
